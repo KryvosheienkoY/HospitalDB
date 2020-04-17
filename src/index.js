2dd0b1f57873cbc4e4480b083ed07d9e685dadeb
KryvosheienkoY <45287471+KryvosheienkoY@users.noreply.github.com>
2020-04-17 18:04:01 +0300

    deleted extra files, added our_departments view file

--- src/index.js
+++ src/index.js
@@ -10,8 +10,22 @@ app.get('/', (req, res) => {
     res.render('main_view');
 });
 
-app.get('/our_departments', (req, res) => {
-    res.render('our_departments_view');
-});
 
+app.get('/our_departments', function (req, res) {
+    res.render('our_departments_view');
+    // const dep = req.department;
+    // if (dep == "all") {
+    //     con.query("SELECT * FROM doctor", function (err, result) {
+    //         response.json({"doctors": result});
+    //         console.log("response");
+    //     });
+    // }
+    // else {
+    //     var sql = "Select * FROM doctor WHERE Department_ID IN (SELECT Department_ID from department WHERE Department_Name=?)";
+    //     con.query(sql, [dep], function (err, result) {
+    //         response.json({"doctors": result});
+    //         console.log("response");
+    //     });
+    // }
+});
 
