0d50cb89f6f1f8d229ab699bf9a35f44b6c56f8d
KryvosheienkoY <45287471+KryvosheienkoY@users.noreply.github.com>
2020-04-17 17:52:50 +0300

    changed to ejs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', './src/views');
 app.use('/', express.static(__dirname + '/../public'));
 app.listen(8080);
