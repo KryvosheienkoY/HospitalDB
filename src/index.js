24d118eda96ecae58c726bbf511372703c3b3ae2
USER <meronyk.tetyana@gmail.com>
2020-04-20 17:08:41 +0100

    jwt token added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+// app.set('views', 'C:/Users/USER/WebstormProjects/HospitalDB/src/views');
 app.set('views', './src/views');
 app.use('/', express.static(__dirname + '/../public'));
 app.use('/patient', express.static(__dirname + '/../public'));
@@ -86,25 +87,27 @@ app.post('/register', function (req, res) {
             Patient_eAddress: req.body.emailR,
             Patient_Birthdate: req.body.birthR,
             Patient_Notes: req.body.notesR
-        }, function (err, result) {
+        }, function (err, resu) {
             if (err) {
                 res.json({"result": "Failed. User exists" + err});
             }
             else {
+                console.log(resu);
                 bcrypt.genSalt(saltRounds, function (err, salt) {
                     bcrypt.hash(pass, salt, function (err, hash) {
                         con.query('INSERT INTO user SET ?', {
                             Username: user,
                             Password: hash,
-                            Patient_ID: res.insertId
-                        }, function (err, result) {
+                            Patient_ID: resu.insertId
+                        }, function (err, resul) {
                             if (err) {
                                 console.log(err);
                             }
                             else {
                                 role = 3;
-                                let user = {id: res.insertID};
+                                let user = {role: 3, id: resu.insertId};
                                 const token = jwt.sign(user, Secret);
+                                console.log("token  "+token);
                                 res.render('login_result_patient_view', {
                                     "result": "success",
                                     "token": token
@@ -119,15 +122,48 @@ app.post('/register', function (req, res) {
 });
 
 app.post('/login', function (req, res) {
-    let usern = req.body.usernameL;
+    // let usern = req.body.usernameL;
+    console.log("Trying to login");
     var sql = "SELECT * FROM user WHERE Username=?";
-    con.query(sql, usern, function (err, result) {
+    con.query(sql, req.body.usernameL, function (err, result) {
+        if(err||result.length<1)
+        {
+            console.log("User not found");
+            res.render('login_result_failed_view', {
+                "result": "Failed. User doesn`t exist"
+            });
+        }
+        else {
+            bcrypt.compare( req.body.passL, result[0].Password,function (erro, resul) {
+                console.log("Password check");
+                if(erro)
+                {
+                    console.log("Password doesnt match");
+                    res.render('login_result_failed_view', {
+                        "result": "Failed. Password doesn`t match"
+                    });
+                }
+                    console.log("Password matches");
+                    if (result[0].Patient_ID == null && result[0].Doctor_ID == null) {
+                        let user = {role: 1};
+                        const token = jwt.sign(user, Secret, jwt.HS256);
+                        //TODO render admin view
+                    } else if (result[0].Patient_ID == null) {
+                        let user = {role : 2, id : result[0].Doctor_ID};
+                        const token = jwt.sign(user, Secret);
+                        //    TODO render doctor view
+                    } else {
+                        console.log("Patient login");
+                        const token = jwt.sign({role: 3,id:result[0].Patient_ID}, Secret);
 
-        bcrypt.compare(result.Password, req.body.passL, function (err, result) {
-            let user = {id: result[0].Patient_ID};
-            const token = jwt.sign(user, Secret);
-            res.json({"token": token});
-        });
+                       // console.log("token  "+role+",  "+id);
+                        res.render('login_result_patient_view', {
+                            "result": "success",
+                            "token": token
+                        });
+                    }
+            });
+         }
 
     });
 });
@@ -150,32 +186,23 @@ app.get('/delete/(:table)/(:id)', function (req, res) {
 
 
 app.get("/patient/my_profile", function (req, res) {
-
-    let t;
-    console.log("req.headers");
-    console.log(req.headers);
-
-    let authz = req.headers.Authorization;
-    console.log("req.headers.Authorization");
-    console.log(authz);
-
-    if (typeof authz !== 'undefined') {
-        let authzParts = authz.split(" ");
-        if (authzParts[0] === "Bearer") {
-            t = authzParts[1];
-        }
-    }
-    else{
-       //send fail
-    }
-    console.log("t ");
-    console.log(t);
-    let tok = jwt.decode(t, Secret);
-    let sql = "SELECT * FROM patient WHERE Patient_ID=?";
-    con.query(sql, tok.id, function (err, result) {
-        var sql = "SELECT * FROM analysis WHERE Patient_ID=?";
-        con.query(sql, tok.id, function (err, resul) {
-            res.json({"patient": result, "analysis": result});
+    if (req.headers && req.headers.authorization) {
+        var auth = req.headers.authorization;
+        // console.log("token  "+auth);
+        let {role, id} = jwt.verify(auth, Secret);
+        let sql = "SELECT * FROM patient WHERE Patient_ID=?";
+        con.query(sql, id, function (err, result) {
+            console.log(result[0]);
+            var sql = "SELECT * FROM analysis WHERE Patient_ID=?";
+            con.query(sql, id, function (err, resul) {
+                res.render("patient_myprofile_view", {
+                    "p": result[0],
+                    "analysis": resul
+                });
+            });
         });
-    });
+    }
 });
+
+
+
